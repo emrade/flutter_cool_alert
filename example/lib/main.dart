4a1653807fe1c9e8d4b48d2dbae4d4b3e4efd542
Emmanuel Fache <emrade95@gmail.com>
2020-06-21 03:50:42 +0000

    chore: Updated README

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Cool Alert'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -27,15 +29,120 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final successAlert = _buildButton(
+      onTap: () {
+        CoolAlert.show(
+          context: context,
+          type: CoolAlertType.success,
+          text: "Transaction completed successfully!",
+        );
+      },
+      text: "Success",
+      color: Colors.green,
+    );
+
+    final errorAlert = _buildButton(
+      onTap: () {
+        CoolAlert.show(
+          context: context,
+          type: CoolAlertType.error,
+          title: "Oops...",
+          text: "Sorry, something went wrong",
+        );
+      },
+      text: "Error",
+      color: Colors.red,
+    );
+
+    final warningAlert = _buildButton(
+      onTap: () {
+        CoolAlert.show(
+          context: context,
+          type: CoolAlertType.warning,
+          text: "You just broke protocol",
+        );
+      },
+      text: "Warning",
+      color: Colors.orange,
+    );
+
+    final infoAlert = _buildButton(
+      onTap: () {
+        CoolAlert.show(
+          context: context,
+          type: CoolAlertType.info,
+          text: "Buy two, get one free",
+        );
+      },
+      text: "Info",
+      color: Colors.blue[100],
+    );
+
+    final confirmAlert = _buildButton(
+      onTap: () {
+        CoolAlert.show(
+          context: context,
+          type: CoolAlertType.confirm,
+          text: "Do you want to logout",
+          confirmBtnText: "Yes",
+          cancelBtnText: "No",
+          confirmBtnColor: Colors.green
+        );
+      },
+      text: "Confirm",
+      color: Colors.lightGreen,
+    );
+
+    final loadingAlert = _buildButton(
+      onTap: () {
+        CoolAlert.show(
+          context: context,
+          type: CoolAlertType.loading,
+        );
+      },
+      text: "Loading",
+      color: Colors.grey,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-                    
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            successAlert,
+            errorAlert,
+            warningAlert,
+            infoAlert,
+            confirmAlert,
+            loadingAlert,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton({VoidCallback onTap, String text, Color color}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: MaterialButton(
+        color: color,
+        minWidth: double.infinity,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        onPressed: onTap,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
